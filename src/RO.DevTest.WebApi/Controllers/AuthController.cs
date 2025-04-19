de4ebe0c977d4f57e1cc9fbca60359edac8e1cf7
Samuel Ribeiro <samuel.ribeiro77f@gmail.com>
2025-04-18 22:38:32 -0400

    refactor: Correction in the AuthController route

--- src/RO.DevTest.WebApi/Controllers/AuthController.cs
+++ src/RO.DevTest.WebApi/Controllers/AuthController.cs
@@ -9,9 +9,9 @@ using RO.DevTest.Domain.Abstract;
 namespace RO.DevTest.WebApi.Controllers;
 
 [ApiController]
-[Route("api/v1/auths")]
-[OpenApiTags("Auths")]
-[ApiExplorerSettings(GroupName = "Auths")]
+[Route("api/v1/auth")]
+[OpenApiTags("Auth")]
+[ApiExplorerSettings(GroupName = "Auth")]
 public class AuthController(IMediator mediator) : ControllerBase
 {
     [HttpGet]
